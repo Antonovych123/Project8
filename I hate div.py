7287340e44b3ca36f77ef728c898705d07d3875d
Antonovych123 <vladyslav.yu.antonovych@ukd.edu.ua>
2024-05-12 23:15:24 +0300

    Richar Baxman

---
+++ I hate div.py
@@ -0,0 +1,12 @@
+m = int(input("Enter number 1:"))
+n = int(input("Enter number 2:"))
+
+def kol():
+    if n == 0:
+        k = "Can't divide by zero!"
+        return k
+
+    h = m / n
+    return h
+
+
